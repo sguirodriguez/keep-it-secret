72202618a7a7aa5845c696ac8072ed0dc4965e62
samuel <guilherme.engr@gmail.com>
2026-01-12 19:21:18 -0300

    chore: fix project imports

--- src/index.ts
+++ src/index.ts
@@ -1,4 +1,14 @@
 import 'dotenv/config'
+import { computeSignature } from './factories/signature'
 
-console.log("teste")
+const payload = {
+    name: "Samuel",
+    type: "PAYMENT",
+    date: Date.now()
+}
 
+console.log("payload", payload)
+
+const signatureSHA256 = computeSignature(process.env.SECRET_KEY!, JSON.stringify(payload))
+
+console.log("signatureSHA256", signatureSHA256)
